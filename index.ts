42f6a586124cab5a80a2349e6a51b021fe726383
katya-sy <katya_sy_28@mail.ru>
2025-01-14 10:23:24 +0900

    task 5

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
+// 1
 console.log("Hello World!");
 
+// 2
 const multiply = (a: number, b: number) => a * b;
 console.log(multiply(2, 3));
 
+// 3
 const repeat = (str: string, count: number) => {
   let res = "";
   for (let i = 0; i < count; i++) {
@@ -12,8 +15,13 @@ const repeat = (str: string, count: number) => {
 };
 console.log(repeat("sometext", 2));
 
+// 4
 function getHiddenCard(cardNumber: string, starCount = 4) {
   if (cardNumber.length !== 16) return "Длина номера не соответствует";
   return repeat("*", starCount) + cardNumber.slice(12);
 }
 console.log(getHiddenCard("1234567890123456", 3));
+
+// 5
+const evenNumbers = (num: number) => num % 2 == 0;
+console.log([1, 2, 3, 4, 5, 6, 7, 8].filter(evenNumbers));
